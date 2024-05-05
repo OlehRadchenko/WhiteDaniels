fbfb19cd35cbe0fb79041d13721c03e5d44b09bb
Oleh Radchenko <or435654@gmail.com>
2024-05-05 12:55:13 +0400

    Code optimization
    Code optimization and preparation for form validation

--- src/LoginAndRegisterForm.js
+++ src/LoginAndRegisterForm.js
@@ -11,137 +11,136 @@ import { useState } from 'react';
 
 const LoginAndRegisterForm = () =>{
     const [mode, setMode] = useState('Login');
-    const [login, setLogin] = useState('');
+    const [email_login, setLogin] = useState('');
     const [password, setPassword] = useState('');
+    const [imie, setImie] = useState('');
+    const [nazwisko, setNazwisko] = useState('');
+    const [dataUro, setDataUro] = useState('');
+    const [email, setEmail] = useState('');
+    const [kraj, setKraj] = useState('');
+    const [miasto, setMiasto] = useState('');
+    const [adres, setAdres] = useState('');
+    const [kod_pocztowy, setKodPocztowy] = useState('');
+    const [waluta, setWaluta] = useState('');
+    const [nr_tel, setNrTel] = useState('');
+    const waluty = ['PLN', 'EUR', 'USD', 'GBP', 'JPY', 'CNY', 'AUD', 'CAD', 'CHF'];
+    const kraje = [
+        "Argentyna", "Australia", "Belgia", "Brazylia", "Brazylia", "Boliwia", "Kanada",
+        "Chile", "Chiny", "Kongo", "Czechy", "Etiopia", "Filipiny", "Finlandia", "Grecja",
+        "Haiti", "Honduras", "Irak", "Irlandia", "Jamajka", "Japonia", "Jordania", "Kazachstan",
+        "Korea", "Niemcy", "Peru", "Polska", "Portugalia", "Paragwaj", "Słowacja", "Słowenia",
+        "Sudan", "Szwajcaria", "Szwecja", "Turcja", "Ukraina", "USA", "Wenezuela", "Walia",
+        "Wielka Brytania", "Włochy"];
+    let email_login_error, password_error, imie_error, nazwisko_error, dataUro_error, email_error, miasto_error, adres_error, kod_pocztowy_error, nr_tel_error;
     const changeMode = () =>{
         setMode(mode === 'Login' ? 'Register' : 'Login');
     }
 
-    const LoginInput = (event) =>{
+    const EmailLoginInput = (event) =>{
         setLogin(event.target.value);
     }
     const PasswordInput = (event) =>{
         setPassword(event.target.value);
     }
     const LoginButton = () =>{
-        if(login !== '' && password !== ''){
-            console.log('Login: ' + login + ' \nPassword: ' + password); //Sprawdzenie czy w bazie danych istnieje taki login i zweryfikowanie poprawności hasła
-        }else if(login === '' && password === ''){
+        if(email_login !== '' && password !== ''){
+            console.log('Login: ' + email_login + ' \nPassword: ' + password); //Sprawdzenie czy w bazie danych istnieje taki login i zweryfikowanie poprawności hasła
+        }else if(email_login === '' && password === ''){
             console.log('Wszystkie dane wprowadzone są puste');
-        }else if(login === ''){
+        }else if(email_login === ''){
             console.log("Wpisany login jest pusty");
         }else{
             console.log("Wpisane hasło jest puste");
         }
     }
 
+    const ImieInput = (event) =>{
+        setImie(event.target.value);
+    }
+    const NazwiskoInput = (event) =>{
+        setNazwisko(event.target.value);
+    }
+    const DataUroInput = (event) =>{
+        setDataUro(event.target.value);
+    }
+    const EmailInput = (event) =>{
+        setEmail(event.target.value);
+    }
+    const KrajInput = (event) =>{
+        setKraj(event.target.value);
+    }
+    const MiastoInput = (event) =>{
+        setMiasto(event.target.value);
+    }
+    const AdresInput = (event) =>{
+        setAdres(event.target.value);
+    }
+    const KodPocztowyInput = (event) =>{
+        setKodPocztowy(event.target.value);
+    }
+    const WalutaInput = (event) =>{
+        setWaluta(event.target.value);
+    }
+    const NrTelInput = (event) =>{
+        setNrTel(event.target.value);
+    }
     const RegisterButton = () =>{
         //Walidacja danych oraz zapis do bazy danych
+        console.log(imie + '\n' + nazwisko + '\n' + dataUro + '\n' + email + '\n' + kraj + '\n' + miasto + '\n' + adres + '\n' + kod_pocztowy + '\n' + waluta + '\n' + nr_tel);
     }
-    
-    if(mode === 'Login'){
-        return(
-            <div id="Login">
-                <div id="titleDiv">
-                    <p id="title">WHITEDANIELS BLACKJACK</p>
-                </div>
-                <form>
-                    <Input placeholder="Login" onChange={LoginInput}></Input>
-                    <Input placeholder="Hasło" onChange={PasswordInput}></Input>
-                    <Button variant="contained" onClick={LoginButton} endIcon={<SendIcon />}>Zaloguj</Button>
-                    <Button variant="outlined" onClick={changeMode}>Nie masz konta? Zarejestruj się poprzez naciśnięcie tego przycisku</Button>
-                </form>
+    return(
+        <div id={mode === 'Login' ? 'Login' : 'Register'}>
+            <div id="titleDiv">
+                <p id="title">WHITEDANIELS BLACKJACK</p>
             </div>
-        );
-    }else{
-        return(
-            <div id="Register">
-                <div id="titleDiv">
-                    <p id="title">WHITEDANIELS BLACKJACK</p>
-                </div>
-                <form action="" method="POST">
-                    <div id="forms">
-                    <div id="form1">
-                        <Input placeholder="Imie"/>
-                        <Input placeholder="Nazwisko"/>
-                        <FormControl fullWidth>
-                            <Input type="date" placeholder="data urodzenia"/>
-                        </FormControl>
-                        <Input placeholder="email"/>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Kraj</InputLabel>
-                            <Select label="Kraj" labelId="demo-simple-select-label" id="demo-simple-select">
-                                <MenuItem value="Argentyna">Argentyna</MenuItem>
-                                <MenuItem value="Australia">Australia</MenuItem>
-                                <MenuItem value="Belgia">Belgia</MenuItem>
-                                <MenuItem value="Brazylia">Brazylia</MenuItem>
-                                <MenuItem value="Brazylia">Brazylia</MenuItem>
-                                <MenuItem value="Boliwia">Boliwia</MenuItem>
-                                <MenuItem value="Kanada">Kanada</MenuItem>
-                                <MenuItem value="Chile">Chile</MenuItem>
-                                <MenuItem value="Chiny">Chiny</MenuItem>
-                                <MenuItem value="Kongo">Kongo</MenuItem>
-                                <MenuItem value="Czechy">Czechy</MenuItem>
-                                <MenuItem value="Etiopia">Etiopia</MenuItem>
-                                <MenuItem value="Filipiny">Filipiny</MenuItem>
-                                <MenuItem value="Finlandia">Finlandia</MenuItem>
-                                <MenuItem value="Grecja">Grecja</MenuItem>
-                                <MenuItem value="Haiti">Haiti</MenuItem>
-                                <MenuItem value="Honduras">Honduras</MenuItem>
-                                <MenuItem value="Irak">Irak</MenuItem>
-                                <MenuItem value="Irlandia">Irlandia</MenuItem>
-                                <MenuItem value="Jamajka">Jamajka</MenuItem>
-                                <MenuItem value="Japonia">Japonia</MenuItem>
-                                <MenuItem value="Jordania">Jordania</MenuItem>
-                                <MenuItem value="Kazachstan">Kazachstan</MenuItem>
-                                <MenuItem value="Korea">Korea</MenuItem>
-                                <MenuItem value="Niemcy">Niemcy</MenuItem>
-                                <MenuItem value="Peru">Peru</MenuItem>
-                                <MenuItem value="Polska">Polska</MenuItem>
-                                <MenuItem value="Portugalia">Portugalia</MenuItem>
-                                <MenuItem value="Paragwaj">Paragwaj</MenuItem>
-                                <MenuItem value="Słowacja">Słowacja</MenuItem>
-                                <MenuItem value="Słowenia">Słowenia</MenuItem>
-                                <MenuItem value="Sudan">Sudan</MenuItem>
-                                <MenuItem value="Szwajcaria">Szwajcaria</MenuItem>
-                                <MenuItem value="Szwecja">Szwecja</MenuItem>
-                                <MenuItem value="Turcja">Turcja</MenuItem>
-                                <MenuItem value="Ukraina">Ukraina</MenuItem>
-                                <MenuItem value="USA">USA</MenuItem>
-                                <MenuItem value="Wenezuela">Wenezuela</MenuItem>
-                                <MenuItem value="Walia">Walia</MenuItem>
-                                <MenuItem value="Wielka Brytania">Wielka Brytania</MenuItem>
-                                <MenuItem value="Włochy">Włochy</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
-                    <div id="form2">
-                        <Input placeholder="Miasto"/>
-                        <Input placeholder="Adres"/>
-                        <Input placeholder="Kod pocztowy"/>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Waluta</InputLabel>
-                            <Select label="Waluta" labelId="demo-simple-select-label" id="demo-simple-select">
-                                <MenuItem value="PLN">PLN</MenuItem>
-                                <MenuItem value="EUR">EUR</MenuItem>
-                                <MenuItem value="USD">USD</MenuItem>
-                                <MenuItem value="GBP">GBP</MenuItem>
-                                <MenuItem value="JPY">JPY</MenuItem>
-                                <MenuItem value="CNY">CNY</MenuItem>
-                                <MenuItem value="AUD">AUD</MenuItem>
-                                <MenuItem value="CAD">CAD</MenuItem>
-                                <MenuItem value="CHF">CHF</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <Input placeholder="numer telefonu"/>
-                    </div>
-                    </div>
-                    <Button variant="contained" color="success" onClick={RegisterButton}>Zarejestruj się</Button>
-                    <Button variant="outlined" onClick={changeMode}>Masz już konto? Zaloguj się poprzez naciśnięcie tego przycisku</Button>
-                </form>
-            </div>
-        );
-    }
+            <form>
+                {mode === 'Login' ? (
+                    <>
+                        <Input placeholder="Email" onChange={EmailLoginInput} />
+                        <Input placeholder="Hasło" type="password" onChange={PasswordInput} />
+                    </>
+                ) : (
+                    <>
+                        <div id="forms">
+                            <div id="form1">
+                                <Input placeholder="Imie" onChange={ImieInput}/>
+                                <Input placeholder="Nazwisko" onChange={NazwiskoInput}/>
+                                <FormControl fullWidth>
+                                    <Input type="date" placeholder="Data urodzenia" onChange={DataUroInput}/>
+                                </FormControl>
+                                <Input placeholder="Email" onChange={EmailInput}/>
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-label">Kraj</InputLabel>
+                                    <Select value={kraj} label="Kraj" labelId="demo-simple-select-label" id="demo-simple-select" onChange={KrajInput}>
+                                        {kraje.map((kraj, index) => <MenuItem key={index} value={kraj}>{kraj}</MenuItem>)}
+                                    </Select>
+                                </FormControl>
+                            </div>
+                            <div id="form2">
+                                <Input placeholder="Miasto" onChange={MiastoInput}/>
+                                <Input placeholder="Adres" onChange={AdresInput}/>
+                                <Input placeholder="Kod pocztowy" onChange={KodPocztowyInput}/>
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-label">Waluta</InputLabel>
+                                    <Select value={waluta} label="Waluta" labelId="demo-simple-select-label" id="demo-simple-select" onChange={WalutaInput}>
+                                        {waluty.map((waluta, index) => <MenuItem key={index} value={waluta}>{waluta}</MenuItem>)}
+                                    </Select>
+                                </FormControl>
+                                <Input placeholder="Numer telefonu" onChange={NrTelInput}/>
+                            </div>
+                        </div>
+                    </>
+                )}
+                <Button variant="contained" color="success" onClick={mode === 'Login' ? RegisterButton : RegisterButton}>
+                    {mode === 'Login' ? 'Zaloguj' : 'Zarejestruj się'}
+                    {mode === 'Login' ? <SendIcon /> : null}
+                </Button>
+                <Button variant="outlined" onClick={changeMode}>
+                    {mode === 'Login' ? 'Nie masz konta? Zarejestruj się poprzez naciśnięcie tego przycisku' : 'Masz już konto? Zaloguj się poprzez naciśnięcie tego przycisku'}
+                </Button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginAndRegisterForm;
