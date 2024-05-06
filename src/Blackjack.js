1426bc13d73fe947a05aee49b382dab617e71157
Oleh Radchenko <or435654@gmail.com>
2024-05-07 00:56:16 +0400

    Improved blackjack.js
    Improved operation of draws and basic blackjack rules
    
    Co-Authored-By: 1Turbox1 <90248065+1Turbox1@users.noreply.github.com>
    Co-Authored-By: OlesPedchenko <75900096+OlesPedchenko@users.noreply.github.com>

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -26,58 +26,58 @@ const Blackjack = () =>{
         { wartosc: 1, ilosc: 0, color: 'Biały' }
     ];
     const karty = [
-        {wartosc: 11, karta: 'As', color: 'Pika'},
-        {wartosc: 10, karta: 'Król', color: 'Pika'},
-        {wartosc: 10, karta: 'Dama', color: 'Pika'},
-        {wartosc: 10, karta: 'Walet', color: 'Pika'},
-        {wartosc: 10, karta: '10', color: 'Pika'},
-        {wartosc: 9, karta: '9', color: 'Pika'},
-        {wartosc: 8, karta: '8', color: 'Pika'},
-        {wartosc: 7, karta: '7', color: 'Pika'},
-        {wartosc: 6, karta: '6', color: 'Pika'},
-        {wartosc: 5, karta: '5', color: 'Pika'},
-        {wartosc: 4, karta: '4', color: 'Pika'},
-        {wartosc: 3, karta: '3', color: 'Pika'},
-        {wartosc: 2, karta: '2', color: 'Pika'},
-        {wartosc: 11, karta: 'As', color: 'Karo'},
-        {wartosc: 10, karta: 'Król', color: 'Karo'},
-        {wartosc: 10, karta: 'Dama', color: 'Karo'},
-        {wartosc: 10, karta: 'Walet', color: 'Karo'},
-        {wartosc: 10, karta: '10', color: 'Karo'},
-        {wartosc: 9, karta: '9', color: 'Karo'},
-        {wartosc: 8, karta: '8', color: 'Karo'},
-        {wartosc: 7, karta: '7', color: 'Karo'},
-        {wartosc: 6, karta: '6', color: 'Karo'},
-        {wartosc: 5, karta: '5', color: 'Karo'},
-        {wartosc: 4, karta: '4', color: 'Karo'},
-        {wartosc: 3, karta: '3', color: 'Karo'},
-        {wartosc: 2, karta: '2', color: 'Karo'},
-        {wartosc: 11, karta: 'As', color: 'Trefl'},
-        {wartosc: 10, karta: 'Król', color: 'Trefl'},
-        {wartosc: 10, karta: 'Dama', color: 'Trefl'},
-        {wartosc: 10, karta: 'Walet', color: 'Trefl'},
-        {wartosc: 10, karta: '10', color: 'Trefl'},
-        {wartosc: 9, karta: '9', color: 'Trefl'},
-        {wartosc: 8, karta: '8', color: 'Trefl'},
-        {wartosc: 7, karta: '7', color: 'Trefl'},
-        {wartosc: 6, karta: '6', color: 'Trefl'},
-        {wartosc: 5, karta: '5', color: 'Trefl'},
-        {wartosc: 4, karta: '4', color: 'Trefl'},
-        {wartosc: 3, karta: '3', color: 'Trefl'},
-        {wartosc: 2, karta: '2', color: 'Trefl'},
-        {wartosc: 11, karta: 'As', color: 'Serce'},
-        {wartosc: 10, karta: 'Król', color: 'Serce'},
-        {wartosc: 10, karta: 'Dama', color: 'Serce'},
-        {wartosc: 10, karta: 'Walet', color: 'Serce'},
-        {wartosc: 10, karta: '10', color: 'Serce'},
-        {wartosc: 9, karta: '9', color: 'Serce'},
-        {wartosc: 8, karta: '8', color: 'Serce'},
-        {wartosc: 7, karta: '7', color: 'Serce'},
-        {wartosc: 6, karta: '6', color: 'Serce'},
-        {wartosc: 5, karta: '5', color: 'Serce'},
-        {wartosc: 4, karta: '4', color: 'Serce'},
-        {wartosc: 3, karta: '3', color: 'Serce'},
-        {wartosc: 2, karta: '2', color: 'Serce'}
+        {wartosc: 11, karta: 'As', color: 'Pika', show: false},
+        {wartosc: 10, karta: 'Król', color: 'Pika', show: false},
+        {wartosc: 10, karta: 'Dama', color: 'Pika', show: false},
+        {wartosc: 10, karta: 'Walet', color: 'Pika', show: false},
+        {wartosc: 10, karta: '10', color: 'Pika', show: false},
+        {wartosc: 9, karta: '9', color: 'Pika', show: false},
+        {wartosc: 8, karta: '8', color: 'Pika', show: false},
+        {wartosc: 7, karta: '7', color: 'Pika', show: false},
+        {wartosc: 6, karta: '6', color: 'Pika', show: false},
+        {wartosc: 5, karta: '5', color: 'Pika', show: false},
+        {wartosc: 4, karta: '4', color: 'Pika', show: false},
+        {wartosc: 3, karta: '3', color: 'Pika', show: false},
+        {wartosc: 2, karta: '2', color: 'Pika', show: false},
+        {wartosc: 11, karta: 'As', color: 'Karo', show: false},
+        {wartosc: 10, karta: 'Król', color: 'Karo', show: false},
+        {wartosc: 10, karta: 'Dama', color: 'Karo', show: false},
+        {wartosc: 10, karta: 'Walet', color: 'Karo', show: false},
+        {wartosc: 10, karta: '10', color: 'Karo', show: false},
+        {wartosc: 9, karta: '9', color: 'Karo', show: false},
+        {wartosc: 8, karta: '8', color: 'Karo', show: false},
+        {wartosc: 7, karta: '7', color: 'Karo', show: false},
+        {wartosc: 6, karta: '6', color: 'Karo', show: false},
+        {wartosc: 5, karta: '5', color: 'Karo', show: false},
+        {wartosc: 4, karta: '4', color: 'Karo', show: false},
+        {wartosc: 3, karta: '3', color: 'Karo', show: false},
+        {wartosc: 2, karta: '2', color: 'Karo', show: false},
+        {wartosc: 11, karta: 'As', color: 'Trefl', show: false},
+        {wartosc: 10, karta: 'Król', color: 'Trefl', show: false},
+        {wartosc: 10, karta: 'Dama', color: 'Trefl', show: false},
+        {wartosc: 10, karta: 'Walet', color: 'Trefl', show: false},
+        {wartosc: 10, karta: '10', color: 'Trefl', show: false},
+        {wartosc: 9, karta: '9', color: 'Trefl', show: false},
+        {wartosc: 8, karta: '8', color: 'Trefl', show: false},
+        {wartosc: 7, karta: '7', color: 'Trefl', show: false},
+        {wartosc: 6, karta: '6', color: 'Trefl', show: false},
+        {wartosc: 5, karta: '5', color: 'Trefl', show: false},
+        {wartosc: 4, karta: '4', color: 'Trefl', show: false},
+        {wartosc: 3, karta: '3', color: 'Trefl', show: false},
+        {wartosc: 2, karta: '2', color: 'Trefl', show: false},
+        {wartosc: 11, karta: 'As', color: 'Serce', show: false},
+        {wartosc: 10, karta: 'Król', color: 'Serce', show: false},
+        {wartosc: 10, karta: 'Dama', color: 'Serce', show: false},
+        {wartosc: 10, karta: 'Walet', color: 'Serce', show: false},
+        {wartosc: 10, karta: '10', color: 'Serce', show: false},
+        {wartosc: 9, karta: '9', color: 'Serce', show: false},
+        {wartosc: 8, karta: '8', color: 'Serce', show: false},
+        {wartosc: 7, karta: '7', color: 'Serce', show: false},
+        {wartosc: 6, karta: '6', color: 'Serce', show: false},
+        {wartosc: 5, karta: '5', color: 'Serce', show: false},
+        {wartosc: 4, karta: '4', color: 'Serce', show: false},
+        {wartosc: 3, karta: '3', color: 'Serce', show: false},
+        {wartosc: 2, karta: '2', color: 'Serce', show: false}
     ];
     const input_obstawianie = (event) =>{
         setObstawione(event.target.value);
@@ -101,28 +101,63 @@ const Blackjack = () =>{
         setMoney(money - wartosc);
     }
 
-    const wylosujKarte = () =>{
+    const wylosujKarte = (shows) =>{
         let karta = Math.floor(Math.random() * karty.length);
-        return karty[karta];
+        let los = karty[karta];
+        los.show = shows;
+        return los;
     }
     const newGame = () =>{
         setMoney(1000);
         setObstawione(0);
         setObstawione2(0);
         setCzyli('');
-        setDealerCards([wylosujKarte(), wylosujKarte()]);
-        setPlayerCards([wylosujKarte(), wylosujKarte()]);
+        setDealerCards([wylosujKarte(true), wylosujKarte(false)]);
+        setPlayerCards([wylosujKarte(true), wylosujKarte(true)]);
         setStartGame(true);
     }
     const hit = () =>{
-        setPlayerCards([...playerCards, wylosujKarte()]);
+        let nowa_karta = wylosujKarte(true);
+        setPlayerCards([...playerCards, nowa_karta]); //raz na jakiś czas, karta nie zostaje dodana dlatego pojawia się bug
+        if(playerCards.reduce((suma, karta) => suma + karta.wartosc, 0)+nowa_karta.wartosc>21){
+            endGame();
+        }
     }
     const stand = () =>{
         setDealerCards([...dealerCards, wylosujKarte()]);
+        endGame();
     }
     const double = () =>{
         setPlayerCards([...playerCards, wylosujKarte()]);
+        endGame();
+    }
+    const endGame = () =>{
         setStartGame(false);
+        showDealerCards();
+        let dealerSum = dealerCards.reduce((suma, karta) => suma + karta.wartosc, 0);
+        if (dealerSum < 17) {
+            const newDealerCards = [...dealerCards];
+            while (dealerSum < 17) {
+                const newCard = wylosujKarte(true);
+                newDealerCards.push(newCard);
+                dealerSum += newCard.wartosc;
+            }
+            setDealerCards(newDealerCards);
+        }
+        let playerSum = playerCards.reduce((suma, karta) => suma + karta.wartosc, 0);
+        if((playerSum > dealerSum && playerSum <= 21) || (dealerSum > 21 && playerSum <= 21)){
+            console.log("WYGRAŁEŚ BOTA, DOSTAJESZ X2 $      DEALER: "+dealerSum+"   PLAYER: "+playerSum);
+        }else if((playerSum > 21 && dealerSum > 21) || (playerSum === dealerSum)){
+            console.log("REMIS!! NIC NIE ZARABIASZ      DEALER: "+dealerSum+"   PLAYER: "+playerSum);
+        }else{
+            console.log("PRZEGRAŁEŚ!! STRACIŁEŚ CAŁOŚĆ      DEALER: "+dealerSum+"   PLAYER: "+playerSum);
+        }
+    }
+    const showDealerCards = () =>{
+        let karty = [...dealerCards];
+        karty[0].show = true;
+        karty[1].show = true;
+        setDealerCards(karty);
     }
     return(
         <div id="Blackjack">
@@ -142,8 +177,8 @@ const Blackjack = () =>{
             </div>
             <div id = "losowania">
                 <Button onClick={newGame}>New Game</Button>
-                <p>Dealer: {dealerCards.map((karta, index) => <span key={index}>{karta.karta} {karta.color} </span>)}</p>
-                <p>Player: {playerCards.map((karta, index) => <span key={index}>{karta.karta} {karta.color} </span>)}</p>
+                <p>Dealer: {dealerCards.map((karta, index) => <span style={karta.show ? {} : {display: 'none'}} key={index}>{karta.karta} {karta.color} </span>)}</p><p style={!startGame ? {} : {display: 'none'}}>punkty dealera: {dealerCards.reduce((suma, karta) => suma + karta.wartosc, 0)}</p>
+                <p>Player: {playerCards.map((karta, index) => <span style={karta.show ? {} : {display: 'none'}} key={index}>{karta.karta} {karta.color} </span>)}</p><p>twoje punkty: {playerCards.reduce((suma, karta) => suma + karta.wartosc, 0)}</p>
                 <Button onClick={hit} color="success" disabled={playerCards.length === 5 || !startGame}>Hit</Button>
                 <Button onClick={stand} color="error" disabled={!startGame}>Stand</Button>
                 <Button onClick={double} color="success" disabled={playerCards.length === 5 || !startGame}>Double</Button>
