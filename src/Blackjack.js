91250160e5b320e27e6c6d4e73ef25efb2cc3ae3
Oleh Radchenko <or435654@gmail.com>
2024-05-06 23:41:50 +0400

    Basic operation of card drawing

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -6,6 +6,11 @@ const Blackjack = () =>{
     const [obstawione, setObstawione] = useState(0);
     const [obstawione2, setObstawione2] = useState(0);
     const [czyli, setCzyli] = useState('');
+
+    const [startGame, setStartGame] = useState(false);
+    const [dealerCards, setDealerCards] = useState([]);
+    const [playerCards, setPlayerCards] = useState([]);
+    
     const nominaly = [
         { wartosc: 5000, ilosc: 0, color: 'Brązowy' },
         { wartosc: 2000, ilosc: 0, color: 'Jasno niebieski' },
@@ -19,6 +24,60 @@ const Blackjack = () =>{
         { wartosc: 10, ilosc: 0, color: 'Niebieski' },
         { wartosc: 5, ilosc: 0, color: 'Czerwony' },
         { wartosc: 1, ilosc: 0, color: 'Biały' }
+    ];
+    const karty = [
+        {wartosc: 11, karta: 'As', color: 'Pika'},
+        {wartosc: 10, karta: 'Król', color: 'Pika'},
+        {wartosc: 10, karta: 'Dama', color: 'Pika'},
+        {wartosc: 10, karta: 'Walet', color: 'Pika'},
+        {wartosc: 10, karta: '10', color: 'Pika'},
+        {wartosc: 9, karta: '9', color: 'Pika'},
+        {wartosc: 8, karta: '8', color: 'Pika'},
+        {wartosc: 7, karta: '7', color: 'Pika'},
+        {wartosc: 6, karta: '6', color: 'Pika'},
+        {wartosc: 5, karta: '5', color: 'Pika'},
+        {wartosc: 4, karta: '4', color: 'Pika'},
+        {wartosc: 3, karta: '3', color: 'Pika'},
+        {wartosc: 2, karta: '2', color: 'Pika'},
+        {wartosc: 11, karta: 'As', color: 'Karo'},
+        {wartosc: 10, karta: 'Król', color: 'Karo'},
+        {wartosc: 10, karta: 'Dama', color: 'Karo'},
+        {wartosc: 10, karta: 'Walet', color: 'Karo'},
+        {wartosc: 10, karta: '10', color: 'Karo'},
+        {wartosc: 9, karta: '9', color: 'Karo'},
+        {wartosc: 8, karta: '8', color: 'Karo'},
+        {wartosc: 7, karta: '7', color: 'Karo'},
+        {wartosc: 6, karta: '6', color: 'Karo'},
+        {wartosc: 5, karta: '5', color: 'Karo'},
+        {wartosc: 4, karta: '4', color: 'Karo'},
+        {wartosc: 3, karta: '3', color: 'Karo'},
+        {wartosc: 2, karta: '2', color: 'Karo'},
+        {wartosc: 11, karta: 'As', color: 'Trefl'},
+        {wartosc: 10, karta: 'Król', color: 'Trefl'},
+        {wartosc: 10, karta: 'Dama', color: 'Trefl'},
+        {wartosc: 10, karta: 'Walet', color: 'Trefl'},
+        {wartosc: 10, karta: '10', color: 'Trefl'},
+        {wartosc: 9, karta: '9', color: 'Trefl'},
+        {wartosc: 8, karta: '8', color: 'Trefl'},
+        {wartosc: 7, karta: '7', color: 'Trefl'},
+        {wartosc: 6, karta: '6', color: 'Trefl'},
+        {wartosc: 5, karta: '5', color: 'Trefl'},
+        {wartosc: 4, karta: '4', color: 'Trefl'},
+        {wartosc: 3, karta: '3', color: 'Trefl'},
+        {wartosc: 2, karta: '2', color: 'Trefl'},
+        {wartosc: 11, karta: 'As', color: 'Serce'},
+        {wartosc: 10, karta: 'Król', color: 'Serce'},
+        {wartosc: 10, karta: 'Dama', color: 'Serce'},
+        {wartosc: 10, karta: 'Walet', color: 'Serce'},
+        {wartosc: 10, karta: '10', color: 'Serce'},
+        {wartosc: 9, karta: '9', color: 'Serce'},
+        {wartosc: 8, karta: '8', color: 'Serce'},
+        {wartosc: 7, karta: '7', color: 'Serce'},
+        {wartosc: 6, karta: '6', color: 'Serce'},
+        {wartosc: 5, karta: '5', color: 'Serce'},
+        {wartosc: 4, karta: '4', color: 'Serce'},
+        {wartosc: 3, karta: '3', color: 'Serce'},
+        {wartosc: 2, karta: '2', color: 'Serce'}
     ];
     const input_obstawianie = (event) =>{
         setObstawione(event.target.value);
@@ -41,6 +100,30 @@ const Blackjack = () =>{
         setObstawione2(obstawione2 + wartosc);
         setMoney(money - wartosc);
     }
+
+    const wylosujKarte = () =>{
+        let karta = Math.floor(Math.random() * karty.length);
+        return karty[karta];
+    }
+    const newGame = () =>{
+        setMoney(1000);
+        setObstawione(0);
+        setObstawione2(0);
+        setCzyli('');
+        setDealerCards([wylosujKarte(), wylosujKarte()]);
+        setPlayerCards([wylosujKarte(), wylosujKarte()]);
+        setStartGame(true);
+    }
+    const hit = () =>{
+        setPlayerCards([...playerCards, wylosujKarte()]);
+    }
+    const stand = () =>{
+        setDealerCards([...dealerCards, wylosujKarte()]);
+    }
+    const double = () =>{
+        setPlayerCards([...playerCards, wylosujKarte()]);
+        setStartGame(false);
+    }
     return(
         <div id="Blackjack">
             <p>Twoje pieniadze: {money} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CHEAT BUTTON = <Button onClick={() => setMoney(money + 1000)}>.</Button></p>
@@ -56,6 +139,14 @@ const Blackjack = () =>{
 
                 </p>
                 Czyli obstawiłeś: {obstawione2}
+            </div>
+            <div id = "losowania">
+                <Button onClick={newGame}>New Game</Button>
+                <p>Dealer: {dealerCards.map((karta, index) => <span key={index}>{karta.karta} {karta.color} </span>)}</p>
+                <p>Player: {playerCards.map((karta, index) => <span key={index}>{karta.karta} {karta.color} </span>)}</p>
+                <Button onClick={hit} color="success" disabled={playerCards.length === 5 || !startGame}>Hit</Button>
+                <Button onClick={stand} color="error" disabled={!startGame}>Stand</Button>
+                <Button onClick={double} color="success" disabled={playerCards.length === 5 || !startGame}>Double</Button>
             </div>
         </div>
     )
